668a22cb50fc50f4d7cd837ac91423c398cfbfbd
Marcelo Augusto <marceloaugusto11@hotmail.com.br>
2020-08-10 15:10:09 -0300

    challenge finalized

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -7,10 +7,22 @@ import FindOrderService from '@modules/orders/services/FindOrderService';
 
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { id } = request.body;
+
+    const findOrder = container.resolve(FindOrderService);
+
+    const order = await findOrder.execute({ id });
+
+    return response.json(order);
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { customer_id, products } = request.body;
+
+    const creteOrder = container.resolve(CreateOrderService);
+
+    const order = await creteOrder.execute({ customer_id, products });
+
+    return response.json(order);
   }
 }
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
+import { response } from 'express';
 import Product from '../infra/typeorm/entities/Product';
 import IProductsRepository from '../repositories/IProductsRepository';
 
@@ -13,10 +14,25 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const checkProduct = await this.productsRepository.findByName(name);
+
+    if (checkProduct) {
+      throw new AppError('Product already registered.');
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
